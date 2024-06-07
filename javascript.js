6fce2aa9f2dc35e22155b4201412f70dc0b952a7
Sylvia <164382913+888-slv@users.noreply.github.com>
2024-06-07 12:40:04 +0100

    code created and fully functional

--- javascript.js
+++ javascript.js
@@ -1,1 +1,49 @@
-console.log("Hello World")
+
+//create computer choice function//
+function getComputerChoice() {
+
+let number = Math.ceil(Math.random() * 10)
+
+
+if (number == 1 || number == 2 || number == 3 ) {
+    result = "paper";
+  } else if (number == 4 || number == 5 || number == 6 ) {
+    result = "rock";
+  } else {
+    result = "scissors";
+
+  }
+}
+//create human choice function//
+
+
+let humanChoice = prompt('What do you choose?');
+
+function getHumanChoice(){
+    return humanChoice;
+}
+
+//ask human and log answer but also convert it to lowercase - this will make the comparison simpler//
+
+getHumanChoice();
+humanChoice = humanChoice.toLowerCase();
+console.log(humanChoice);
+
+//run game and log result then compare results and alert results
+
+getComputerChoice();
+console.log(result);
+
+
+
+if ((humanChoice === 'rock' && result === 'paper') || (humanChoice === 'paper' && result === 'scissors') || (humanChoice === 'scissors' && result === 'rock')){
+        alert('You loose');
+    } else if ((humanChoice === 'rock' && result === 'scissors') || (humanChoice === 'paper' && result === 'rock') || (humanChoice === 'scissors' && result === 'paper')) {
+        alert('You win')
+     }     else {
+        alert('Its a tie.')
+     }
+
+
+
+
